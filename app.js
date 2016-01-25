2ba9215114e46f8d833d66d868e27b8eedcfa2a5
sinhvis <sinhvis@gmail.com>
2016-01-25 15:49:57 -0800

    Added factory for posts

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var app = angular.module('newsApp', []) ;
 
-app.controller('MainCtrl', ['$scope', function($scope) {
-	$scope.test = 'Hello World!' ;
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	} ;
+	return o ;
+}])
 
-	$scope.oldPosts = [
-		'post 1',
-		'post 2',
-		'post 3',
-		'post 4',
-		'post 5'
-	] ;
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	] ;
+	// $scope.posts = [
+	// 	{title: 'post 1', upvotes: 5},
+	// 	{title: 'post 2', upvotes: 2},
+	// 	{title: 'post 3', upvotes: 15},
+	// 	{title: 'post 4', upvotes: 9},
+	// 	{title: 'post 5', upvotes: 4}
+	// ] ;
+
+	$scope.posts = posts.posts ;
 
 	$scope.addPost = function() {
 		// prevent user from submitting post with blank title
