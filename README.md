34b64b1cfbdecb138a7115f57dfe16d17e03eedc
sinhvis <sinhvis@gmail.com>
2016-01-26 18:14:26 -0800

    Changed directory structure. Added node backend

---
+++ README.md
@@ -0,0 +1,12 @@
+## To Install
+npm install
+
+bower install
+
+# Run
+> node app.js
+
+Make sure mongoDB is running
+
+* Persistent
+-- saves to: mongodb://localhost/news
--- app.js
+++ app.js
@@ -1,46 +1,66 @@
-var app = angular.module('newsApp', []) ;
+var express = require('express');
+var path = require('path');
+// var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.factory('posts', [function() {
-	var o = {
-		posts: []
-	} ;
-	return o ;
-}])
+//MongoDB connecting and modeling:
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
 
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+require('./models/Posts');
+require('./models/Comments');
 
-	$scope.posts = posts.posts ;
+var routes = require('./routes/index');
+var app = express();
 
-	$scope.posts = [
-	{title: 'post 1', upvotes: 5},
-	{title: 'post 2', upvotes: 2},
-	{title: 'post 3', upvotes: 15},
-	{title: 'post 4', upvotes: 9},
-	{title: 'post 5', upvotes: 4},
-	{title: 'post 6 with link',
-	link: 'http://www.yahoo.com', upvotes: 3},
-	{title: 'post 7 with link',
-	link: 'http://www.google.com', upvotes: 4}
-	] ;
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-	
-	$scope.addPost = function() {
-		// prevent user from submitting post with blank title
-		if (!$scope.title || $scope.title === '') { return ; }
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public_html')));
 
-		$scope.posts.push({ 
-			title:		$scope.title, 
-			link:		$scope.link,
-			upvotes: 	0 
-		}) ;
+app.use('/', routes);
+//app.use('/users', users);
 
-		// need to clear the post and link after the post 
-		// has been created.
-		$scope.title = '' ;
-		$scope.link = '' ;
-	} ;
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1 ;
-	} ;
-}]) ;
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-c to terminate');
+});
+module.exports = app;
